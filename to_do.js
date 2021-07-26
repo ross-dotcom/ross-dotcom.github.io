3440048f0afeacf52d985d3050cff81f2af34e5e
ross-dotcom <wross843@gmail.com>
2021-07-26 16:49:19 +0200

    Debugging.

--- to_do.js
+++ to_do.js
@@ -1,8 +1,20 @@
-function checkEmpty() {
+/**function checkEmpty() {
     if (document.getElementById("input-task").value.trim() == "") {
         alert("Please add a task");
     }
     else {
         alert("First if not working!");
+    }
+}*/
+
+function myFunction() {
+    
+    var text = document.getElementById("idea").value;
+    if (text == "") {
+        alert("Please add a task");
+    }
+    else {
+        var li = "<li>" + text + "</li>";
+        document.getElementById("list").appendChild(li);
     }
 }
