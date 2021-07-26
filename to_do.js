431ff3877af54e2c94ce214289a30153a79fc1ab
ross-dotcom <wross843@gmail.com>
2021-07-26 17:15:41 +0200

    Debugging.

--- to_do.js
+++ to_do.js
@@ -3,14 +3,11 @@
 function myFunction() {
     
     var text = document.getElementById("idea").value;
-    var checkbox = document.createElement("INPUT");
     
     if (text == "") {
         alert("Please add a task");
     }
     else {
-        checkbox.setAttribute("type", "checkbox");
-        document.body.appendChild(checkbox);
         var li = document.createElement("li");
         li.innerHTML = text;
         document.getElementById("list").appendChild(li);
