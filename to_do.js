3e2bc86ff29d300deca4f3361d3fa242d3ac5b3d
ross-dotcom <wross843@gmail.com>
2021-07-27 20:24:09 +0200

    Creating checkbox.

--- to_do.js
+++ to_do.js
@@ -11,5 +11,9 @@ function myFunction() {
         var li = document.createElement("li");
         li.innerHTML = text;
         document.getElementById("list").appendChild(li);
+        
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+        document.body.appendChild(x);
     }
 }
