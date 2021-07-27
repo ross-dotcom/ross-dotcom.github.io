fa733e142d71b32f977d4f24b54adaf45cefe114
ross-dotcom <wross843@gmail.com>
2021-07-27 20:52:24 +0200

    Creating checkbox: debugging.

--- to_do.js
+++ to_do.js
@@ -10,12 +10,6 @@ function myFunction() {
     else {
         var li = document.createElement("li");
         li.innerHTML = text;
-        //document.getElementById("list").appendChild(li);
-        
-        var liCheckBox = document.createElement("input");
-        liCheckBox.type = "checkbox";
-        
-        li.innerHTML += liCheckbox.outerHTML;
         document.getElementById("list").appendChild(li);
     }
 }
