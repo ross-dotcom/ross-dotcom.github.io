95af0bdccb4a6866b21c49a48a4095cc6277db6c
ross-dotcom <wross843@gmail.com>
2021-07-28 20:17:52 +0200

    Testing: checkbox.

--- to_do.js
+++ to_do.js
@@ -1,6 +1,6 @@
 /* exported myFunction */
 
-/*function myFunction() {
+function myFunction() {
     
     var text = document.getElementById("idea").value;
     
@@ -12,24 +12,4 @@
         li.innerHTML = text;
         document.getElementById("list").appendChild(li);
     }
-}*/
-
-function myFunction() {
-    
-    var newLi = document.createElement('li');
-    
-    var checkBox = cb = document.createElement("input");
-    cb.type = "checkbox";
-    cb.id = "c1";
-    cb.value = name;
-    cb.checked = false;
-    
-    newLi.appendChild(cb);
-    
-    var text = document.createTextNode(name);
-    
-    newLi.appendChild(text);
-    
-    var ul = document.getElementById("idea");
-    ul.appendChild(newLi);
 }
