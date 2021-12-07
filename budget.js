913d87414c1d4280335ec6183c715216474fe8bb
ross-dotcom <wross843@gmail.com>
2021-12-07 20:27:10 +0200

    Added 'amount' next to 'Item' in HTML and Javascript.

--- budget.js
+++ budget.js
@@ -3,19 +3,11 @@
 /* exported removeItem */
 /* exported display_time */
 
-function display_time() {
-
-    /*n = new Date();
-    y = n.getFullYear;
-    m = n.getMonth()+1;
-    d = n.getDate();*/
-    //document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
-    document.getElementById("date").innerHTML = Date();
-}
-
 function myFunction() {
     
     var text = document.getElementById("idea").value;
+    
+    var amt = document.getElementById("amount").value;
     
     if (text == "") {
         alert("Please add a task");
@@ -25,10 +17,6 @@ function myFunction() {
         li.innerHTML = text;
         
         var cb = document.createElement("input");
-        cb.type = "checkbox";
-        cb.value = text;
-        cb.checked = false;
-        li.appendChild(cb);
         
         document.getElementById("list").appendChild(li);
     }
