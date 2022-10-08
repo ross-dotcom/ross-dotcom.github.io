db251c014dda5cebf013b13f84d7abcd374933f0
ross-dotcom <wross843@gmail.com>
2022-10-08 23:08:14 +0200

    Completed Calculator app using HTML, CC and JavaScript. Working on layout using BS5 Grids.

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,3 @@
-function add() {
-    let a = 1;
-    let b = 2;
-    let c = a + b;
-    document.getElementById("display").innerHTML = c;
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
 }
