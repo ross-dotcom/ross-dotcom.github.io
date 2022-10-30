fc37c9d5d84e628358a5fe9e3bab8711a366d98c
ross-dotcom <wross843@gmail.com>
2022-10-30 14:24:25 +0200

    The following changes were made: 1. Width updated to 50%, 2. 'To Do Web App' to 'Task List', 3. Added Quote of the day: in HTML I added a p element and calls a JS function that gets a quote from an array according to which day it is, 3. Added day to date, 4. Added event listener to X button to remove individual items from list, 4. Clear input field on button click.

--- to_do.js
+++ to_do.js
@@ -1,25 +1,25 @@
 /* exported myFunction */
 /* exported removeAll */
 /* exported removeItem */
-/* exported display_time */
+/* exported display_the_time */
+/* exported enterToAdd */
+/* exported quote_of_the_day */
+/* exported daily_quotes */
 
-/*function display_time() {
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    n = new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate();
-    document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
-}*/
+const today = new Date();
+var day = weekday[today.getDay()];
+var date = day + ', ' + today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
 
-function display_the_time() {
 
-    let today = new Date();
-    let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+function display_the_time(){
+      
     document.getElementById("date").innerHTML = date;
 }
 
-function myFunction() {
+
+function myFunction(){
     
     var text = document.getElementById("idea").value;
     
@@ -36,16 +36,29 @@ function myFunction() {
         cb.checked = false;
         li.appendChild(cb);
         
+        var xb = document.createElement("BUTTON");
+        var x = document.createTextNode("x");
+        xb.appendChild(x);
+        
+        li.appendChild(xb);
+        
         document.getElementById("list").appendChild(li);
+        document.getElementById("idea").value = ""; //Clear input field on click.
+        
+        xb.addEventListener("click", function(){
+            li.remove();
+        });
+        
     }
 }
 
-function removeAll() {
+
+function removeAll(){
     
     document.getElementById("list").innerHTML = "";
 }
 
-function enterToAdd () {
+function enterToAdd(){
     var input = document.getElementById("idea");
     input.addEventListener("keypress", function(event) {
         if (event.key === "Enter") {
@@ -53,4 +66,10 @@ function enterToAdd () {
             document.getElementById("add").click();
         }
     });
+}
+
+var daily_quotes = ["\"Discipline will take you to places where motivation can't.\"", "\"Remember your goal and why you started.\"", "\"Make big plans, enjoy the little things.\"", "\"We don't grow when we stay inside our comfort zone.\"", "\"Consistency is key!\"", "\"There is a past version of you that is so proud of how far you've come.\"", "\"Don't give up, show up everyday!\""];
+
+function quote_of_the_day(){
+    document.getElementById("quote").innerHTML = daily_quotes[today.getDay()];
 }
