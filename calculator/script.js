eb1065d2727adb33d9fbed41b169df408c1f6cc6
ross-dotcom <wross843@gmail.com>
2022-11-05 21:19:32 +0200

    HTML, CSS and JavaScript changes.

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,11 @@
+/* exported calcNumbers */
+/* exported clear_all */
+/* exported percent */
+
 function calcNumbers(result){
     form.displayResult.value=form.displayResult.value+result;
+}
+
+function clear_all(){
+    form.displayResult.value=null;
 }
