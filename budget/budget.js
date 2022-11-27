331a7ed5659a8594af6e7c16c83b0b83b055f232
ross-dotcom <wross843@gmail.com>
2022-11-27 17:16:12 +0200

    Added a new table for Total Expense and Amount Remaining.

--- budget/budget.js
+++ budget/budget.js
@@ -1,12 +1,13 @@
 /* exported submit_total_amount */
 /* exported myFunction */
+/* exported submit_expense_amount */
 
 var expense_name = "";
 var amount = 0;
 
 function submit_total_amount(){
     if (document.getElementById("total_amount").value == ""){
-        alert("Please enter a total amount");
+        document.getElementById("insert_total").value = 0;
     }
     else {
         document.getElementById("insert_total").innerHTML = document.getElementById("total_amount").value;
@@ -14,10 +15,18 @@ function submit_total_amount(){
     }
 }
 
+function add_row(){
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = expense_name;
+    cell2.innerHTML = amount;
+}
+
 function myFunction(){
     
     var text = document.getElementById("xp").value;
-    
     var amt_text = document.getElementById("xa").value;
     
     if (text == "") {
@@ -32,50 +41,19 @@ function myFunction(){
         
         var a = document.createElement("p");
         a.innerHTML = amt_text;
-        /*li.appendChild(a);*/
-        
-        /*var xb = document.createElement("BUTTON");
-        var x = document.createTextNode("x");
-        xb.appendChild(x);
-        
-        li.appendChild(xb);*/
-        
-        //document.getElementById("list").appendChild(li);
+    
         document.getElementById("xp").value = ""; //Clear input field on click.
         document.getElementById("xa").value = ""; //Clear input field on click.
-        
-        /*xb.addEventListener("click", function(){
-            li.remove();
-        });*/
         
         expense_name = text;
         amount = amt_text;
         
         add_row();
+        //submit_expense_amount();
     }
 }
 
-
-function removeAll(){
-    
-    document.getElementById("list").innerHTML = "";
+function submit_expense_amount(){
+    //document.getElementById("insert_expense").innerHTML += parseInt(amount);
+    //document.getElementById("total_amount").value = "";
 }
-
-function add_row(){
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = expense_name;
-    cell2.innerHTML = amount;
-}
-
-/*function enterToAdd(){
-    var input = document.getElementById("xp");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("add").click();
-        }
-    });
-}*/
