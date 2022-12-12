55cf1e0ec06d7bdf8f86b3cbd2c099322891f63a
ross-dotcom <wross843@gmail.com>
2022-12-12 22:00:07 +0200

    Log in page completed: only requires email address and password to log in. Using JS to alert when either email address or password field is empty. Home page created and in progress!

--- twitter_clone/script.js
+++ twitter_clone/script.js
@@ -1,5 +1,16 @@
 /* exported submit */
 
+let store_email = '';
+
 function submit(){
-  console.log(document.getElementById("pass1").value);
+  if (document.getElementById("validationDefault01").value != '' &&  document.getElementById("validationDefault02").value != ''){
+    store_email = document.getElementById("validationDefault01").value;
+    window.location.href = "home.html";
+  } else if (document.getElementById("validationDefault01").value == ''){
+    alert("Email address is required!");
+  } else {
+      if (document.getElementById("validationDefault02").value == ''){
+        alert("Password is required!");
+      }
+  }
 }
