a629f3a5d35efa7b6a7b49b4a017b691605ba544
ross-dotcom <wross843@gmail.com>
2022-12-20 21:13:31 +0200

    JavaScript: 'Tweet' button onclick to add Tweet/(s) in progress.

--- twitter_clone/script.js
+++ twitter_clone/script.js
@@ -1,5 +1,28 @@
 /* exported alertOnLoad */
+/* exported myFunction */
 
 function alertOnLoad(){
-  alert('Please note that this project is in progress and not fully completed.')
+  alert('Please note that this project is in progress and not fully completed.');
+}
+
+function myFunction(){
+    
+    var user = 'user123';
+    
+    var h = document.createElement("h4");
+    h.innerHTML = user;
+    
+    var text = document.getElementById("exampleFormControlTextarea1").value;
+    
+    var li = document.createElement("p");
+    li.innerHTML = text;
+    
+    li.appendChild(h);
+    
+    document.getElementById("list").appendChild(li);
+    document.getElementById("exampleFormControlTextarea1").value = ""; //Clear input field on click.
+    
+    var line = document.createElement("HR");
+    
+    document.getElementById("list").appendChild(line);
 }
