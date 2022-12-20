89295ef31699d36ba3e36eb6964f440ddecba05b
ross-dotcom <wross843@gmail.com>
2022-12-20 20:04:24 +0200

    JavaScript: Alert message on load.

--- twitter_clone/script.js
+++ twitter_clone/script.js
@@ -1,16 +1,5 @@
-/* exported submit */
+/* exported alertOnLoad */
 
-let store_email = '';
-
-function submit(){
-  if (document.getElementById("validationDefault01").value != '' &&  document.getElementById("validationDefault02").value != ''){
-    store_email = document.getElementById("validationDefault01").value;
-    window.location.href = "home.html";
-  } else if (document.getElementById("validationDefault01").value == ''){
-    alert("Email address is required!");
-  } else {
-      if (document.getElementById("validationDefault02").value == ''){
-        alert("Password is required!");
-      }
-  }
+function alertOnLoad(){
+  alert('Please note that this project is in progress and not fully completed.')
 }
