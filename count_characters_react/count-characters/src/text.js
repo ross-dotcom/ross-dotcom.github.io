0567af68103588ab047d10eea5278b57274123d3
ross-dotcom <wross843@gmail.com>
2023-02-27 21:36:31 +0200

    Added 'Reset Result' and 'Clear All' buttons.

--- count_characters_react/count-characters/src/text.js
+++ count_characters_react/count-characters/src/text.js
@@ -7,13 +7,24 @@ function TypeText(props) {
     document.getElementById("text-field").value = null;
   };
   
+  const resetResultPressed = () => {
+    document.getElementById("count").innerHTML = 0;
+  };
+  
+  const clearAllPressed = () => {
+    document.getElementById("text-field").value = null;
+    document.getElementById("count").innerHTML = 0;
+  };
+  
   return (
     <div>
       <form>
-        <input id="text-field" type="text" size="100" height="200px" value={txt} onChange={(e) => setText(e.target.value)}/>
+        <input id="text-field" type="text" size="100" height="200px" placeholder="Type text here..." value={txt} onChange={(e) => setText(e.target.value)}/>
       </form>
-      <p>Count: {txt.length}</p>
+      <h4>Count: <p id="count">{txt.length}</p></h4>
       <button type="button" onClick={clearTextPressed}>Clear Text</button>
+      <button type="button" onClick={resetResultPressed}>Reset Result</button>
+      <button type="button" onClick={clearAllPressed}>Clear All</button>
     </div>
   )
 }
