6b0816f4972392e12a720f079104fe23f5419f6f
ross-dotcom <wross843@gmail.com>
2023-02-27 21:29:37 +0200

    Added 'Clear text box button'.

--- count_characters_react/count-characters/src/text.js
+++ count_characters_react/count-characters/src/text.js
@@ -3,16 +3,17 @@ import { useState } from "react";
 function TypeText(props) {
   const [txt, setText] = useState("");
   
-  //const searchButtonPressed = () => {
-  //  console.log(name);
-  //};
+  const clearTextPressed = () => {
+    document.getElementById("text-field").value = null;
+  };
   
   return (
     <div>
       <form>
-        <input id="text-field" type="text" value={txt} onChange={(e) => setText(e.target.value)}/>
+        <input id="text-field" type="text" size="100" height="200px" value={txt} onChange={(e) => setText(e.target.value)}/>
       </form>
       <p>Count: {txt.length}</p>
+      <button type="button" onClick={clearTextPressed}>Clear Text</button>
     </div>
   )
 }
