8f45e83a98530f994a38177cd38b4946c61f1a4c
Wesley <wross843@gmail.com>
2024-05-26 18:31:13 +0200

    Updates

--- myScript.js
+++
@@ -1,8 +0,0 @@
-function buttonFunction() {
-    document.getElementById('myImage').src='./images/smile_after.png';
-    document.getElementById("myBtn").disabled = true;
-}
-
-function myFunction() {
-    document.getElementById('testImage').src = './images/smile_after.png';
-}
